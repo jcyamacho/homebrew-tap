657643ab7a938878e40856f807c071752e9916de
Juan C. Yamacho H. <jcyamacho@gmail.com>
2026-09-01 23:40:27 +0200

    memo 0.0.7
    Update memo formula to 0.0.7.

--- Formula/memo.rb
+++ Formula/memo.rb
@@ -1,8 +1,8 @@
 class Memo < Formula
   desc "Durable memory CLI for humans and coding agents"
   homepage "https://github.com/jcyamacho/memo"
-  url "https://github.com/jcyamacho/memo/archive/refs/tags/v0.0.6.tar.gz"
-  sha256 "5e2218b5a7989d2a4729ca24843334d486dbbe176b8e2e2b9fe1acd25925094d"
+  url "https://github.com/jcyamacho/memo/archive/refs/tags/v0.0.7.tar.gz"
+  sha256 "51aed67b2a46709fe2b99991747822abed84ff6de6d709c7e50c519d0fe08f9d"
 
   depends_on "go" => :build
 
